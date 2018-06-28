b323692840a0cf06c7a15506f6e624e490bd0159
Johny Scott <j.scott@kainos.com>
2018-06-28 10:22:13 +0100

    Employee Class

---
+++ ThursdayProject/src/Employee.java
@@ -0,0 +1,75 @@
+
+public class Employee {
+	
+	private String name;
+	private String address;
+	private String NIN;
+	private int accNo;
+	private double salary;
+	private int empNo;
+	
+	public Employee(String name, String address, String nIN, int accNo, double salary, int empNo) {
+		super();
+		this.name = name;
+		this.address = address;
+		NIN = nIN;
+		this.accNo = accNo;
+		this.salary = salary;
+		this.empNo = empNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNIN() {
+		return NIN;
+	}
+
+	public void setNIN(String nIN) {
+		NIN = nIN;
+	}
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	
+	
+	
+	
+
+	
+
+}
