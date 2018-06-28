12672fd5db0e521ce7e6dc94e4d706d9fbdc67e2
Johny Scott <j.scott@kainos.com>
2018-06-28 16:02:00 +0100

    Updated the tester

--- ThursdayProject/src/DbTester.java
+++ ThursdayProject/src/DbTester.java
@@ -8,22 +8,29 @@ public class DbTester {
 	
 	public static void main(String args[]) {
 		
+		int ans;
+		do {
+		System.out.println("Choose an option: \n1.Insert Employee\n2.Insert Dept.\n3.Select employees from dept\n0.To exit the program");
+		ans = sc.nextInt();
 		
-		
-		System.out.println("Choose an option: 1.Insert Employee\n2.Insert Dept.\n3.Select employees from dept");
-		int ans = sc.nextInt();
 		sc.nextLine();
 		
 		switch(ans) {
-			case 1 : insertEmp();
-			case 2 : insertDept();
+			case 1 : insertEmp(); break;
+			case 2 : insertDept(); break;
 			case 3 : 
 				System.out.println("Insert dept name");
 				String deptName = sc.nextLine();
 				selectEmpsFromDept(deptName);
+				break;
+			default: ;
 		}
 		
+		}while(ans != 0);
+		System.exit(0);
 	}
+		
+	
 
 
 	public static void insertEmp(){
@@ -44,7 +51,17 @@ public class DbTester {
 			System.out.println("Insert postcode of employee: ");
 			String postcode = sc.nextLine();
 			
+			System.out.println("Is this Employee a Sales Employee? Y or N: ");
+			String ans = sc.nextLine();
+			if(ans.equals("Y")) {
+				System.out.println("Please enter commission rate: ");
+				float comRate = sc.nextFloat();
+				System.out.println("Please enter total sales: ");
+				float totalSales = sc.nextFloat();
+			} else {
+			
 			db.insertEmployee(name, salary, NIN, sortCode, accNo, address, postcode);
+			}
 	}
 	
 	public static void insertDept() {
