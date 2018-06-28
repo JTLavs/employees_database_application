8f4015225950fe0055c0b607a28050654fb0ae5a
James Lavery <james.lavery@kainos.com>
2018-06-28 15:22:34 +0100

    Modify DbTester

--- ThursdayProject/src/DbTester.java
+++ ThursdayProject/src/DbTester.java
@@ -3,48 +3,63 @@ import java.util.Scanner;
 
 public class DbTester {
 	
-	public static Scanner sc = new Scanner(System.in);;
+	public static Scanner sc = new Scanner(System.in);
+	static ConnectionDB db = new ConnectionDB();
 	
 	public static void main(String args[]) {
 		
-		ConnectionDB db = new ConnectionDB();
 		
-		System.out.println("Do you want to insert and employee or dept? Press D for dept and E for employee");
-		String ans = sc.nextLine();
-		if(ans.contains("E")) {
 		
-		System.out.println("Insert name of employee: ");
-		String name = sc.nextLine();
-		System.out.println("Insert salary of employee: ");
-		float salary = sc.nextFloat();
-		System.out.println("Insert National Insurance Number of employee: ");
+		System.out.println("Choose an option: 1.Insert Employee\n2.Insert Dept.\n3.Select employees from dept");
+		int ans = sc.nextInt();
 		sc.nextLine();
-		String NIN = sc.nextLine();
-		System.out.println("Insert sort code of employee: ");
-		String sortCode = sc.nextLine();
-		System.out.println("Insert Account Number of employee: ");
-		int accNo = sc.nextInt();
-		System.out.println("Insert address of employee (eg: Number Street, Town, County): ");
-		sc.nextLine();
-		String address = sc.nextLine();
-		System.out.println("Insert postcode of employee: ");
-		String postcode = sc.nextLine();
 		
-	
-		db.insertEmployee(name, salary, NIN, sortCode, accNo, address, postcode);
-		
-		} else if(ans.equals("D")) {
-			
-			System.out.println("Please enter Department name: ");
-			String dept_name = sc.nextLine();
-			
-			db.insertDepartment(dept_name);
-			
-			
-		} else {
-			System.out.println("Incorrect input. Exited");
+		switch(ans) {
+			case 1 : insertEmp();
+			case 2 : insertDept();
+			case 3 : 
+				System.out.println("Insert dept name");
+				String deptName = sc.nextLine();
+				selectEmpsFromDept(deptName);
 		}
 		
+	}
+
+
+	public static void insertEmp(){
+			System.out.println("Insert name of employee: ");
+			String name = sc.nextLine();
+			System.out.println("Insert salary of employee: ");
+			float salary = sc.nextFloat();
+			System.out.println("Insert National Insurance Number of employee: ");
+			sc.nextLine();
+			String NIN = sc.nextLine();
+			System.out.println("Insert sort code of employee: ");
+			String sortCode = sc.nextLine();
+			System.out.println("Insert Account Number of employee: ");
+			int accNo = sc.nextInt();
+			System.out.println("Insert address of employee (eg: Number Street, Town, County): ");
+			sc.nextLine();
+			String address = sc.nextLine();
+			System.out.println("Insert postcode of employee: ");
+			String postcode = sc.nextLine();
+			
+			db.insertEmployee(name, salary, NIN, sortCode, accNo, address, postcode);
+	}
+	
+	public static void insertDept() {
+		System.out.println("Please enter Department name: ");
+		String dept_name = sc.nextLine();
+		
+		db.insertDepartment(dept_name);
+	}
+	
+	public static void selectEmpsFromDept(String name) {
+		ArrayList<String> deptEm = db.selectEmployeesFromDept(name);
+		
+		for(String e : deptEm) {
+			System.out.println(e);
+		}
 	}
 
 }
