c53bab6fcc569903b449707bd226dd34467f6ecf
James Lavery <james.lavery@kainos.com>
2018-06-28 14:03:23 +0100

    Modify DbTester file

--- ThursdayProject/src/DbTester.java
+++ ThursdayProject/src/DbTester.java
@@ -7,10 +7,6 @@ public class DbTester {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Press y to show employees in the database");
-		String response = sc.nextLine();
-		ArrayList<Employee> employees = ConnectionDB.getEmployees();
-		
 		System.out.println("Insert name of employee: ");
 		String name = sc.nextLine();
 		System.out.println("Insert salary of employee: ");
